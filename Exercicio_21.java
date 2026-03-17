0c922a198b0938583ab4e3b3869734b9f4ca8b7c
Dorival-Camara <garfixsd@gmail.com>
2026-03-17 14:58:10 -0300

    Exercicio 21

---
+++ Exercicio_21.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class teatro{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner
+
+        int[] assentos = [12][12];
+
+        System.out.println("Informe a sala do espetáculo sendo 1- (Chopin), 2- (Mozart) : ");
+        int espetaculo = scanner.nextInt();
+
+        System.out.println("");
+        
+    }
+}
