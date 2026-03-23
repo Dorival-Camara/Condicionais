a12d5e14de3794cb8319ae83a16da7345c775fad
Dorival_Camara <garfixsd@gmail.com>
2026-03-23 18:43:28 -0300

    Add files via upload

--- Exercicio_21.java
+++ Exercicio_21.java
@@ -1,16 +1,39 @@
 import java.util.Scanner;
 
-public class teatro{
+public class Exercicio_21{
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner
+        Scanner scanner = new Scanner(System.in);
 
-        int[] assentos = [12][12];
+        System.out.println("Digite o nome da sala : ");
+        String nomeSala = scanner.nextLine();
 
-        System.out.println("Informe a sala do espetáculo sendo 1- (Chopin), 2- (Mozart) : ");
-        int espetaculo = scanner.nextInt();
+        System.out.println("Digite o nome do espetaculo");
+        String nomeEspetaculo = scanner.nextLine();
 
-        System.out.println("");
+        char[][] cadeiras = new char[12][12];
+
+        for(int i = 0; i < 12; i++){
+            for(int c = 0; c < 12; c++){
+                cadeiras[i][c] = 'L';
+                
+            }
+        }
         
+        for(int i = 0; i < 12; i++){
+            for(int c = 0; c < 12; c++){
+                if(cadeiras[i][c] == 'L'){
+                    System.out.print("[ ]");
+                }  
+                else if(cadeiras[i][c] == 'R'){
+                    System.out.print("[R]");
+                }
+                else{
+                    System.out.print("[X]");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
